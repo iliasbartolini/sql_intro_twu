dd8122b36b38a39eee2b5210890b6a24a324e1f3
Ilias Bartolini <ibartoli@thoughtworks.com>
2012-07-06 14:31:01 +0530

    updates 2nd day

--- 03_INSERT_SELECT_DELETE.sql
+++ 03_INSERT_SELECT_DELETE.sql
@@ -1,10 +1,13 @@
 
 INSERT INTO TERMS (name, start, end) VALUES ('twu 27', '2012-07-01', '2012-07-24');
+
 INSERT INTO TERMS (name, start, end) VALUES ('twu 28', '2012-08-01', '2012-08-24');
 
 SELECT * FROM TERMS;
 
 INSERT INTO TERMS (name, start, end) VALUES ('twu 27', '2012-07-01', '2012-07-24');
+
+SELECT * FROM TERMS;
 
 DELETE FROM TERMS;
 
--- 05_JOIN.sql
+++ 05_JOIN.sql
@@ -1,7 +1,49 @@
 
-CREATE TABLE PARTICIPANT (
-name VARCHAR( 100 ) ,
+CREATE TABLE PARTICIPANTS (
+name VARCHAR( 100 ) primary key,
 term VARCHAR( 100 )
 );
 
+INSERT INTO PARTICIPANTS (name, term) VALUES ('Agalya L', 'twu 27');
+
+INSERT INTO PARTICIPANTS (name, term) VALUES ('April', 'twu 27');
+
+INSERT INTO PARTICIPANTS (name, term) VALUES ('Ashok', 'twu 27');
+
+INSERT INTO PARTICIPANTS (name, term) VALUES ('Roy', 'twu 28');
+
+INSERT INTO PARTICIPANTS (name, term) VALUES ('Trevor', 'twu 28');
+
+SELECT * FROM PARTICIPANTS;
+
+--projection
+SELECT name FROM PARTICIPANTS;
+
+--ORDER BY
+SELECT * FROM PARTICIPANTS
+ORDER BY name;
+
+SELECT * FROM PARTICIPANTS
+ORDER BY name DESC;
+
+--WHERE
+SELECT * FROM PARTICIPANTS WHERE term = 'twu 27';
+
+--count(), min(), max()
+SELECT count(*) FROM PARTICIPANTS WHERE term = 'twu 27';
+SELECT max(term) FROM PARTICIPANTS;
+SELECT min(name) FROM PARTICIPANTS WHERE term = 'twu 27';
+
+--JOIN
+SELECT * FROM 
+PARTICIPANTS p, TERMS t
+WHERE p.term = t.name
+AND t.start > '2012-07-15';
+
+--GROUP BY 
+SELECT count(*) FROM 
+PARTICIPANTS p, TERMS t
+WHERE p.term = t.name
+AND t.name = 'twu 27'
+GROUP BY t.end;
 
